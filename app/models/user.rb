9a7c95387ba7c83c1e085a2a2b42f38e583057e8
damianhouse <damianhouse@gmail.com>
2016-04-04 18:14:05 -0400

    Add associations. Add has secure password, validates password, email unique.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :events
+  validates :email, presence: true, uniqueness: true
+  has_many :messages, through: :events
+  validates_confirmation_of :password
 end
