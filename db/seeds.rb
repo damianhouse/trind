7cdb35ba1d9c3ea14e129f0ca7456bf9bf87f655
damianhouse <damianhouse@gmail.com>
2016-04-04 21:17:17 -0400

    Add a couple seeds to begin testing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+d = User.create!(name: "Damian House", email: "damianhouse@gmail.com", password: "password")
+q = User.create!(name: "Quentin Hill", email: "quentinhill@gmail.com", password: "password")
+e = Event.create!(user_id: d.id, searcher_id: q.id, interests: "coffee", description: "Grab some coffee", location: "cocoa cinnamon", success: true )
+Message.create!(event_id: e.id, searcher_id: q.id, user_id: d.id, message: "I would love to chat over a latte!", viewed: true)
