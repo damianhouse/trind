47806da8192a24d3105126a42d205ad060b2a8c4
damianhouse <damianhouse@gmail.com>
2016-04-12 15:01:20 -0400

    Change uid to string to account for length.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412151506) do
+ActiveRecord::Schema.define(version: 20160412190039) do
 
   create_table "events", force: :cascade do |t|
     t.integer  "user_id"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 20160412151506) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "token"
-    t.integer  "uid"
+    t.string   "uid"
   end
 
 end
