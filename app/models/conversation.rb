39925db625852821183eeb553f98c3025da3544e
damianhouse <damianhouse@gmail.com>
2016-04-15 09:13:10 -0400

    Add dependent destroy all the way through chain from user to message.

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,6 @@
 class Conversation < ActiveRecord::Base
   belongs_to :event
   belongs_to :sender, class_name: "User"
-  belongs_to :recipient, class_name: "User" 
-  has_many :messages
+  belongs_to :recipient, class_name: "User"
+  has_many :messages, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :events
+  has_many :events, dependent: :destroy
   validates :email, presence: true, uniqueness: true
   has_many :messages, through: :events
-  has_many :conversations_as_sender, class_name: "Conversation", foreign_key: :sender_id
-  has_many :conversations_as_recipient, class_name: "Conversation", foreign_key: :recipient_id
+  has_many :conversations_as_sender, class_name: "Conversation", foreign_key: :sender_id, dependent: :destroy
+  has_many :conversations_as_recipient, class_name: "Conversation", foreign_key: :recipient_id, dependent: :destroy
   validates_confirmation_of :password
 
   def conversations
