7ccb42b85681713c32278d3b6c1076fded5600d6
damianhouse <damianhouse@gmail.com>
2016-05-06 15:49:08 -0400

    Add routes in config/events.rb for messages create and index.

--- config/events.rb
+++ config/events.rb
@@ -11,4 +11,6 @@ WebsocketRails::EventMap.describe do
   #     subscribe :new, :to => ProductController, :with_method => :new_product
   #   end
   # The above will handle an event triggered on the client like `product.new`.
+  subscribe :message_post, 'messages#create'
+  subscribe :messages, 'messages#index'
 end
